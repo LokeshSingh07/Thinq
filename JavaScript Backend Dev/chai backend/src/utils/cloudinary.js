a0c36cb251849fe3e8a0803797d5c548cd7101be
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-06-25 19:16:52 +0530

    V2.6
    multer.middleware & cloudinary

---
+++ JavaScript Backend Dev/chai backend/src/utils/cloudinary.js
@@ -0,0 +1,49 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+// cloudinary config
+;(()=>{
+    try{
+        cloudinary.config({ 
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+            api_key: process.env.CLOUDINARY_API_KEY, 
+            api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+        });
+        console.log("Cloudinary connected successfully");
+    }
+    catch(err){
+        console.log("Cloudinary connection failed");
+    }
+})();
+
+
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try{
+        if(!localFilePath){
+            return res.status(404).json({
+                success: false,
+                message: "file path not found",
+            });            
+        }
+
+        // upload the file to cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+
+        // file has been uloaded successfully
+        console.log("File is uploaded on cloudinary succesfully: ", response.url);
+        
+        return response;
+    }
+    catch(err){
+        console.log("file not uploaded on cloudniary");;
+        fs.unlinkSync(localFilePath);       // remove the locally saved temp file as the upload ooperation got failed
+    }
+}
+
+
+
+export {uploadOnCloudinary}
