386b41c6123f27a1080c603702c33224b92806f2
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-06-25 14:16:42 +0530

    V2.4
    Utils code

--- JavaScript Backend Dev/chai backend/src/app.js
+++ JavaScript Backend Dev/chai backend/src/app.js
@@ -1,5 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
 const app = express();
+
+
+// app.use()    to config middleware
+
+// allow origin to send req
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+app.use(express.json({limit: "16kb"}));        // accept json req
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("pubic"));
+app.use(cookieParser());
 
 
 
