1caf77a107e87771c18645ca8a1490b3834354b5
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-07-01 11:20:10 +0530

    V2.12
    "Added getWatchHistory controller and route, updated user routes and controllers with new imports and functionality"

--- JavaScript Backend Dev/chai backend/src/controllers/user.controller.js
+++ JavaScript Backend Dev/chai backend/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.models.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose';
 
 
 
@@ -485,8 +486,62 @@ const getUserChannelProfile = asyncHandler(async(req,res)=> {
 
 
 
-// 
+const getWatchHistory = asyncHandler(async(req,res)=> {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Schema.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "Video",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "User",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        fullName: 1,
+                                        avatar: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    // owner field ko hi overwrite kr diya h frontend ki sahuliyat ke liye
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0]?.watchHistory,
+            "Watch History fetched successfully"
+        )
+    )
+
+})
 
 
 
@@ -506,5 +561,6 @@ export {
     updateUserAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
+    getWatchHistory,
 
 }; 
